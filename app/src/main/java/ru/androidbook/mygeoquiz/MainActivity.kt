abcdcd74b01abb98eba1b41939b48bc0fa8e27de
IgorNiNo <vis_74@mail.ru>
2021-12-04 15:00:40 +0300

    Initial commit

---
+++ app/src/main/java/ru/androidbook/mygeoquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.androidbook.mygeoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
