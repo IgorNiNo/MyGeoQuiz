76eaf9c6824e244435d67611c6af4553a72c647c
IgorNiNo <vis_74@mail.ru>
2021-12-05 13:16:17 +0300

    devChapter1_commit1

--- app/src/main/java/ru/androidbook/mygeoquiz/MainActivity.kt
+++ app/src/main/java/ru/androidbook/mygeoquiz/MainActivity.kt
@@ -2,10 +2,34 @@ package ru.androidbook.mygeoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener {
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+//            val toastTrue = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)    //Не работает, с версии api 30 и выше
+//            toastTrue.setGravity(Gravity.TOP, 0, 0)
+//            toastTrue.show()
+        }
+
+        falseButton.setOnClickListener {
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+//            val toastFalse = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT) //Не работает, с версии api 30 и выше
+//            toastFalse.setGravity(Gravity.TOP, 0, 0)
+//            toastFalse.show()
+        }
     }
 }
